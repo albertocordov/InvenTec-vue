bc31d945e32688653f6a20712bae6f1f015b3784
albertocordov <18170302@itculiacan.edu.mx>
2023-11-04 17:59:02 -0700

    Vales de resguardo

--- src/views/valesResguardo.vue
+++ src/views/valesResguardo.vue
@@ -1,34 +1,126 @@
 <template>
-    <div class="entrar">
-      <h1>Vales de resguardo</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'valesResguardo',
-    data: () => ({
-      //
-    }),
-    components: {}
-  };
-  </script>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            <h3>VALE ÚNICO DE RESGUARDO DE BIENES MUEBLES</h3>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="UNIDAD RESPONSABLE"
+                  v-model="unidadResponsable"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="FECHA"
+                  v-model="fecha"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="ÁREA"
+                  v-model="area"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="CENTRO DE TRABAJO"
+                  v-model="centroTrabajo"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="DATOS DEL SERVIDOR PÚBLICO"
+                  v-model="datosServidorPublico"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-data-table
+                  :items="bienesAsignados"
+                  :headers="headers"
+                  class="elevation-1"
+                ></v-data-table>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-btn color="primary" @click="imprimirPDF">Imprimir</v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-  <style>
+<script>
 
-.entrar {
-    animation: entrada 0.7s ease;
-}
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+export default {
+  name: "ValeResguardoBienes",
+  data() {
+    return {
+      unidadResponsable: "",
+      fecha: "",
+      area: "",
+      centroTrabajo: "",
+      datosServidorPublico: "",
+      bienesAsignados: [
+        {
+          cant: 1,
+          caracteristica: {
+            noInventario: 1,
+            marca: "",
+            nombre: "",
+            modelo: "",
+            serie: ""
+          },
+          usuario: ""
+        }
+      ]
+    };
+  },
+  methods: {
+    imprimirPDF() {
+      const data = {
+        unidadResponsable: this.unidadResponsable,
+        fecha: this.fecha,
+        area: this.area,
+        centroTrabajo: this.centroTrabajo,
+        datosServidorPublico: this.datosServidorPublico,
+        bienesAsignados: this.bienesAsignados
+      };
 
-@keyframes entrada {
-    from {
-        opacity: 0;
+      const doc = new jsPDF();
+
+      doc.text("SEP", 105, 20, "center");
+      doc.text("VALE ÚNICO DE RESGUARDO DE BIENES MUEBLES", 105, 30, "center");
+      doc.text("Unidad responsable: " + data.unidadResponsable, 20, 60);
+      doc.text("Fecha: " + data.fecha, 20, 70);
+      doc.text("Área: " + data.area, 20, 80);
+      doc.text("Centro de trabajo: " + data.centroTrabajo, 20, 90);
+      doc.text("Datos del servidor público: " + data.datosServidorPublico, 20, 100);
+
+      const rows = data.bienesAsignados.map(bien => [
+        bien.cant,
+        `${bien.caracteristica.noInventario}, ${bien.caracteristica.marca}, ${bien.caracteristica.nombre}, ${bien.caracteristica.modelo}, ${bien.caracteristica.serie}`,
+        bien.usuario
+      ]);
+
+      doc.autoTable({
+        head: [["Cant.", "Característica", "Usuario"]],
+        body: rows,
+        startY: 110,
+        theme: "grid"
+      });
+      doc.save("informe.pdf");
     }
-
-    to {
-        opacity: 100%;
-    }
-}
-
-
-</style>
+  }
+};
+</script>

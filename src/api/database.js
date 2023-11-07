26e32d591590fae1be2ef887c5c9ed251b9fc77b
albertocordov <18170302@itculiacan.edu.mx>
2023-11-06 22:24:18 -0700

    Diseño vales de resguardoy lógica de SQL, express para persistencia de datos

--- src/api/database.js
+++ src/api/database.js
@@ -1,33 +1,63 @@
-const sql = require('mssql'); 
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const config = {
-  server: 'DAVID-D14\SQLEXPRESS',
+  user: 'userinventec',
+  password: '12345678',
+  server: 'DAVID-D14\\SQLEXPRESS',
   database: 'inventec',
   options: {
-    trustedConnection: true, // Habilita Windows Authentication
+    encrypt: false, // Deshabilita SSL/TLS
   },
 };
 
-async function conectarBaseDeDatos() {
+app.get('/api/inventory', async (req, res) => {
   try {
-    await sql.connect(config);
-    console.log('Conexión a la base de datos establecida.');
+    const pool = await sql.connect(config);
+    const result = await pool
+      .request()
+      .query(`SELECT 
+                    A.ActId,
+                    A.ActNombre,
+                    A.ActCaracteristicas,
+                    J.JefeNombre AS JefeDepartamento,
+                    D.DepAlias AS NombreDepartamento
+                FROM Activos AS A
+                INNER JOIN Areas AS AR ON A.AreaId = AR.AreaId
+                INNER JOIN Departamentos AS D ON AR.Depclave = D.Depclave
+                LEFT JOIN Jefes AS J ON D.JefeId = J.JefeId;
+                ;`);
+    res.json(result.recordset);
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+    console.error(error);
+    res.status(500).send(`Error en el servidor: ${error.message}`);
   }
-}
+});
 
-async function obtenerUsuarios() {
+app.get('/api/departments', async (req, res) => {
   try {
-    const result = await sql.query('SELECT id, nombre, correo, tipo_usuario FROM usuarios');
-    return result.recordset;
+    const pool = await sql.connect(config);
+    const result = await pool
+      .request()
+      .query(`SELECT
+            CAST(D.Depclave AS VARCHAR) AS ID, -- Aquí realizamos la conversión a texto (VARCHAR)
+            D.Depdepto AS Departamento,
+          COALESCE(J.JefeNombre, 'SIN ENCARGADO') AS NombreEncargado
+      FROM Departamentos AS D
+      LEFT JOIN Jefes AS J ON D.JefeId = J.JefeId;`);
+    res.json(result.recordset);
   } catch (error) {
-    console.error('Error al obtener usuarios:', error);
-    return [];
+    console.error(error);
+    res.status(500).send(`Error en el servidor: ${error.message}`);
   }
-}
+});
 
-module.exports = {
-  conectarBaseDeDatos,
-  obtenerUsuarios,
-};
+const port = 3000; // El puerto en el que se ejecutará el servidor
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
+});

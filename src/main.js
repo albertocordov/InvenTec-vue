5aa1af1cb5812a9f0962eddb9a46a785c28eb2d6
SaulCastanedaTEC <18170381@itculiacan.edu.mx>
2023-12-05 15:27:04 -0700

    deploy a produccion

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'animate.css';
 
+
 import { initializeApp } from "firebase/app";
 import 'firebase/auth'; 
 import 'firebase/firestore';
